023583e0587fae8b8e6cc78471ab31ee023342d2
Geeta Qltech <geeta@qltech.com.au>
2021-07-28 18:27:47 +0530

    First G B Commit 28-07

---
+++ backup/21-7-2021-brainwellnessspa_live/header-services.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+
+<html lang="en-US" class="no-js no-svg" <?php language_attributes(); ?>>
+
+<head>
+<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-5TNSLT2');</script>
+		<!-- End Google Tag Manager -->
+  <meta charset="utf-8">
+  <meta name="google-site-verification" content="dV0b6wQWw_-MoZhS0-kcYmytsVPdPErMNQTcxUxJqpU" />
+  <meta name="google-site-verification" content="bOjScfYkA8s_Flzg0mq8E6zCfiLSleJRlpVeyOOIAMc" />
+  <meta name="ahrefs-site-verification" content="e4a204ba7add4d164c294ed6ee75da824e7849c5225bc257d454805252ccc664">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+
+  <meta name="format-detection" content="telephone=no">
+  <meta class="foundation-mq">
+  <link rel="icon" href="https://brainwellnessspa.com.au/wp-content/uploads/2020/04/cropped-favicon-32x32.png" sizes="32x32" />
+
+  <!-- <link rel="shortcut icon" type="image/png" href="https://brainwellnessspa.com.au/wp-content/themes/brainwellnessspa/newservices/images/favicon.png" /> -->
+
+  <link rel="stylesheet" type="text/css" href="https://brainwellnessspa.com.au/wp-content/themes/brainwellnessspa/newservices/css/bootstrap.min.css">
+
+  <link rel="stylesheet" type="text/css" href="https://brainwellnessspa.com.au/wp-content/themes/brainwellnessspa/newservices/css/slick.css">
+
+  <link rel="stylesheet" type="text/css" href="https://brainwellnessspa.com.au/wp-content/themes/brainwellnessspa/newservices/css/fontawesome.css">
+
+  <link rel="stylesheet" type="text/css" href="https://brainwellnessspa.com.au/wp-content/themes/brainwellnessspa/newservices/css/navigation.css">
+
+  <link rel="stylesheet" type="text/css" href="https://brainwellnessspa.com.au/wp-content/themes/brainwellnessspa/newservices/css/style.css">
+  
+  <link rel='stylesheet' id='gforms_reset_css-css'  href='https://brainwellnessspa.com.au/wp-content/plugins/gravityforms/css/formreset.min.css?ver=2.4.23' type='text/css' media='all' />
+<link rel='stylesheet' id='gforms_formsmain_css-css'  href='https://brainwellnessspa.com.au/wp-content/plugins/gravityforms/css/formsmain.min.css?ver=2.4.23' type='text/css' media='all' />
+<link rel='stylesheet' id='gforms_ready_class_css-css'  href='https://brainwellnessspa.com.au/wp-content/plugins/gravityforms/css/readyclass.min.css?ver=2.4.23' type='text/css' media='all' />
+<link rel='stylesheet' id='gforms_browsers_css-css'  href='https://brainwellnessspa.com.au/wp-content/plugins/gravityforms/css/browsers.min.css?ver=2.4.23' type='text/css' media='all' />
+
+<link rel='stylesheet' id='foundation-css-css'  href='https://brainwellnessspa.com.au/wp-content/themes/brainwellnessspa/vendor/foundation-sites/dist/css/foundation.min.css?ver=5.4.6' type='text/css' media='all' />
+
+<link rel="stylesheet" type="text/css" href="https://brainwellnessspa.com.au/wp-content/themes/brainwellnessspa/newservices/css/depression_new_style.css">
+<script type='text/javascript' src='https://brainwellnessspa.com.au/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='https://brainwellnessspa.com.au/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+
+  
+
+<?php if($post->post_name == 'depression'){ ?>
+  <title>Depression Treatment Perth | Relieve Depression | Brain Wellness Spa</title>
+  <meta name="description" content="Overcome depression and live your best life. Get drug-free relief for morning/evening/night depressions, major depressions, or just the inability to function." />
+  
+<?php } ?>
+<?php if($post->post_name == 'anger'){ ?>
+  <title>Anger Management Perth | Brain Wellness Spa, Perth</title>
+  <meta name="description" content="Overcome depression and live your best life. Get drug-free relief for morning/evening/night depressions from mild to major symptoms. Book Now." />
+  
+<?php } ?>
+<?php if($post->post_name == 'post-traumatic-stress'){ ?>
+  <title>Post Traumatic Stress Disorder | PTSD Support | Brain Wellness Spa</title>
+  <meta name="description" content="Post-traumatic stress disorder (PTSD) is a mental health disorder that occurs when you experience stress. This is treatable so it’s important to ask for help." />
+  
+<?php } ?>
+<?php if($post->post_name == 'anxiety'){ ?>
+  <title>Anxiety Treatment Perth | Live Anxiety Free | Brain Wellness Spa</title>
+  <meta name="description" content="Natural anxiety clinic at Brain Wellness Spa Perth. Overcome panic attacks, social anxiety, GAD & more. 97.4% feel change after 1st session." />
+  
+<?php } ?>
+
+<?php if($post->post_name == 'home-new'){ ?>
+  <title>Mental Health Services Perth Australia | Brain Wellness Spa</title>
+  <meta name="description" content="100% Drug-Free Mental health services Perth. Alternative Method for Mental Health in Perth, Australia. Helping in inspired Living. Book Now." />
+  
+<?php } ?>
+
+<?php if($post->post_name == 'insomnia'){ ?>
+  <title>Insomnia Disorder Management in Perth | Brain Wellness Spa</title>
+  <meta name="description" content="Insomnia is associated with mental disorders. A common sleep disorder that affects millions of people. Ask our practitioners about managing Insomnia Disorder." />
+  
+<?php } ?>
+
+<?php if($post->post_name == 'borderline-personality-disorder'){ ?>
+  <title>Borderline Personality Disorder (BPD) | Perth Brain Wellness Spa</title>
+  <meta name="description" content="Borderline Personality Disorder is a mental illness that affects mood and behavior. Our Drug free alternative treatment helps to find mental health balance." />
+  
+<?php } ?>
+
+<?php if($post->post_name == 'bipolar'){ ?>
+  <title>Bipolar Disorder Support Services | Brain Wellness Spa Perth</title>
+  <meta name="description" content="Suffering from Bipolar disorder? get significant relief with our drug free treatment in Perth with a 98% success rate with clients diagnosed with Bipolar." />
+  
+<?php } ?>
+
+<?php if($post->post_name == 'burnout'){ ?>
+  <title>Burnout Prevention & Recovery in Perth | Brain Wellness Spa</title>
+  <meta name="description" content="Are you feeling Burned out or mentally exhausted? Find about Career & Fatigue burnout symptoms and Prevent burning yourself so you can remain engaged and happy." />
+  
+<?php } ?>
+
+
+<?php //wp_head(); 
+
+ global $post;?>
+
+</head>
+
+<?php if($post->post_name == 'depression-landing-new' OR $post->post_name == 'depression'){ ?>
+<body class="depressionlanding">
+<?php } ?>
+<?php if($post->post_name == 'anger-landing-new' OR $post->post_name == 'anger'){ ?>
+<body class="angerlanding">
+<?php } ?>
+<?php if($post->post_name == 'ptsd-landing-new' OR $post->post_name == 'post-traumatic-stress'){ ?>
+<body class="ptsdlanding">
+<?php } ?>
+<?php if($post->post_name == 'anxiety-landing-new' OR $post->post_name == 'anxiety'){ ?>
+<body class="anxietylanding">
+<?php } ?>
+<?php if($post->post_name == 'insomnia'){ ?>
+<body class="insomnialanding">
+<?php } ?>
+<?php if($post->post_name == 'borderline-personality-disorder'){ ?>
+<body class="borderlinelanding">
+<?php } ?>
+<?php if($post->post_name == 'bipolar'){ ?>
+<body class="bipolarlanding">
+<?php } ?>
+<?php if($post->post_name == 'burnout'){ ?>
+<body class="burnoutlanding">
+<?php } ?>
+
+
+<div class="site-main">
+
+<noscript>
+
+    <div class="noscript">
+
+    	<p><i class="fa fa-exclamation-triangle fa-1"></i>Please enable JavaScript in your browser.</p>
+
+    </div>
+
+</noscript> 
+<div id="wrapper" class="off-canvas-wrapper">
+			
+			<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+				
+				<?php get_template_part( 'parts/content', 'offcanvas' ); ?>
+				
+				<div class="off-canvas-content" data-off-canvas-content>
+<!--header section starts-->
+
+<?php 
+  get_template_part( 'parts/nav', 'offcanvas-topbarnew' ); 
+ ?>
+
